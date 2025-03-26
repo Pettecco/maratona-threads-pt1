634b80279319a614ba0747c48b4131232c8c2f30
Murilo <106248842+Murilo-de-Oliveira@users.noreply.github.com>
2025-03-25 21:25:25 -0300

    Add files via upload

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,22 +2,26 @@ import com.projetoSO.maratonadeprogramacao.Equipe;
 import com.projetoSO.maratonadeprogramacao.Maratona;
 import com.projetoSO.maratonadeprogramacao.Problema;
 
-public static void main(String[] args) {
-    List<Problema> problemas = new ArrayList<>();
-    Equipe[] equipes = new Equipe[5];
+import java.util.*;
 
-    // Criar problemas e adicioná-los na lista de problemas
-    for (int i = 0; i < 10; i++) {
-        Random random = new Random();
-        int dificuldade = random.nextInt(10) + 1;
-        Problema problema = new Problema(dificuldade);
-        problemas.add(problema);
+public class Main{
+    public static void main(String[] args) {
+        List<Problema> problemas = new ArrayList<>();
+        Equipe[] equipes = new Equipe[5];
+
+        // Criar problemas e adicioná-los na lista de problemas
+        for (int i = 0; i < 10; i++) {
+            Random random = new Random();
+            int dificuldade = random.nextInt(10) + 1;
+            Problema problema = new Problema(dificuldade);
+            problemas.add(problema);
+        }
+
+        for (int i = 0; i < equipes.length; i++) {
+            equipes[i] = new Equipe("Equipe " + (i + 1), new ArrayList<>(problemas));
+        }
+
+        Maratona maratona = new Maratona(equipes, 10);
+        maratona.run();
     }
-
-    for (int i = 0; i < equipes.length; i++) {
-        equipes[i] = new Equipe("Equipe " + (i + 1), problemas);
-    }
-
-    Maratona maratona = new Maratona(equipes, 10);
-    maratona.run();
 }
